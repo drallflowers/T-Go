5eef36385cae3d2140c737da25bdc73888a8dac9
Jacob Spoelstra <jacobs62@iastate.edu>
2017-01-20 13:42:49 -0600

    Added JobBoardActivity.java and activity_job_board.xml

---
+++ app/src/main/java/com/may1722/t_go/ui/JobBoardActivity.java
@@ -0,0 +1,16 @@
+package com.may1722.t_go.ui;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.may1722.t_go.R;
+
+public class JobBoardActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_board);
+    }
+}
