e79343f021ea8bf2e8d4867788c0c6e18778382a
dtlauria <dtlauria@iastate.edu>
2017-01-22 11:43:09 -0600

    added some job submit code. Need to work on time picker some more

---
+++ app/src/main/java/com/may1722/t_go/ui/JobSubmitActivity.java
@@ -0,0 +1,78 @@
+package com.may1722.t_go.ui;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.support.v4.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.app.*;
+import android.support.v4.app.FragmentActivity;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import com.may1722.t_go.R;
+
+import java.util.Calendar;
+
+public class JobSubmitActivity extends FragmentActivity implements
+        DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_submit);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day){
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        // Do something with the time chosen by the user
+    }
+
+    public void pickDate(View view){
+
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+        //Will get setup to send text in UserNameEditText and PasswordEditText to database to check if the user exists
+
+    public static class DatePickerFragment extends DialogFragment{
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState){
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            return new DatePickerDialog(getActivity(), (JobSubmitActivity)getActivity(), year, month, day);
+        }
+    }
+
+public static class TimePickerFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), (JobSubmitActivity)getActivity() , hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+}
+}
+
--- app/src/main/java/com/may1722/t_go/ui/LoginActivity.java
+++ app/src/main/java/com/may1722/t_go/ui/LoginActivity.java
@@ -3,6 +3,7 @@ package com.may1722.t_go.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.content.Intent;
 
 import com.may1722.t_go.R;
 
@@ -19,7 +20,12 @@ public class LoginActivity extends AppCompatActivity {
         //startActivity(intent);                                    //Starts teh Activity we want to go to
     }
 
+
+
     public void login(View view){
+
+        Intent intent = new Intent(this, JobSubmitActivity.class);   //Sets up the class of the Activity we want to go to
+        startActivity(intent);
         //Will get setup to send text in UserNameEditText and PasswordEditText to database to check if the user exists
     }
 }
