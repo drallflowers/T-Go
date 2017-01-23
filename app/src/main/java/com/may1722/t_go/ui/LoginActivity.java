708b0b7d84ebb60a6683323762f67d46b257aa1e
alxdaly <alxdaly@iastate.edu>
2017-01-22 18:01:37 -0600

    Developed the layout for the profile screen and set the orientation of profile and login to be portrait only.

--- app/src/main/java/com/may1722/t_go/ui/LoginActivity.java
+++ app/src/main/java/com/may1722/t_go/ui/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.may1722.t_go.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,11 +16,13 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void goToSignup(View view){
-        //Intent intent = new Intent(this, SignUpActivity.class);   //Sets up the class of the Activity we want to go to
-        //startActivity(intent);                                    //Starts teh Activity we want to go to
+//        Intent intent = new Intent(this, SignUpActivity.class);   //Sets up the class of the Activity we want to go to
+//        startActivity(intent);                                    //Starts teh Activity we want to go to
     }
 
     public void login(View view){
         //Will get setup to send text in UserNameEditText and PasswordEditText to database to check if the user exists
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
     }
 }
