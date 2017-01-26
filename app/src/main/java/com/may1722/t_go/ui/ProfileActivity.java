fe2583994212c0cb8b74b9b4f8ddc698f932eb0b
Jacob Spoelstra <jacobs62@iastate.edu>
2017-01-26 15:55:09 -0600

    Linked ProfileActivity to CurrentJobs, JobSubmit, ReviewBoard, and JobBoard

--- app/src/main/java/com/may1722/t_go/ui/ProfileActivity.java
+++ app/src/main/java/com/may1722/t_go/ui/ProfileActivity.java
@@ -1,7 +1,9 @@
 package com.may1722.t_go.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.may1722.t_go.R;
 
@@ -11,5 +13,25 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void goToCurrentJobs(View view){
+        Intent intent = new Intent(this, CurrentJobs.class);
+        startActivity(intent);
+    }
+
+    public void goToJobSubmit(View view){
+        Intent intent = new Intent(this, JobSubmitActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToReviewBoard(View view){
+        Intent intent = new Intent(this, ReviewBoardActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToJobBoard(View view){
+        Intent intent = new Intent(this, JobBoardActivity.class);
+        startActivity(intent);
     }
 }
