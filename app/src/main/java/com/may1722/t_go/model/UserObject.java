8baadccd4586e620733ab60e3e5ba4881886c357
Phippen <phippen@iastate.edu>
2017-01-30 20:00:57 -0600

    Add user object with starting properties

---
+++ app/src/main/java/com/may1722/t_go/model/UserObject.java
@@ -0,0 +1,12 @@
+package com.may1722.t_go.model;
+
+/**
+ * Created by Michael Phippen on 1/30/2017.
+ */
+
+public class UserObject {
+
+    private int ID;
+    private String username;
+    private String password;
+}
