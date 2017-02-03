f738d7bdfda419eea7695d1ef5bb0ddb27778ad7
Phippen <phippen@iastate.edu>
2017-02-02 19:32:07 -0600

    User object filled with remaining properties from database

--- app/src/main/java/com/may1722/t_go/model/UserObject.java
+++ app/src/main/java/com/may1722/t_go/model/UserObject.java
@@ -1,12 +1,26 @@
 package com.may1722.t_go.model;
 
+import java.sql.Date;
+
 /**
  * Created by Michael Phippen on 1/30/2017.
  */
 
 public class UserObject {
 
-    private int ID;
-    private String username;
+    //Account Info
+    private int userid;
+    private int user_type;
+    private Date date_created;
+    private int default_delivery_address_id;
+
+    //Personal Info
+    private String first_name;
+    private String last_name;
+
+    private String email;
     private String password;
+
+    private String phone_number;
+    private Date date_of_birth;
 }
