5e2b6c82d58e72f1d7a61654883c9463c2ef806f
Phippen <phippen@iastate.edu>
2017-02-02 19:45:44 -0600

    Add a few functions

--- app/src/main/java/com/may1722/t_go/model/CourierObject.java
+++ app/src/main/java/com/may1722/t_go/model/CourierObject.java
@@ -7,4 +7,22 @@ public class CourierObject {
     private int user_id;
     //TODO Change to job object when added
     private ArrayList<Object> currentJobs;
+
+    public void addJob(Object o)
+    {
+        currentJobs.add(o);
+    }
+
+    public void removeJob(Object o)
+    {
+        currentJobs.remove(o);
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
 }
