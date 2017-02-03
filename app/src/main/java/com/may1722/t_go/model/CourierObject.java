027531ccdc7f0d4b25cd521c3f7676894a7afeb6
Phippen <phippen@iastate.edu>
2017-02-02 19:40:56 -0600

    Beginning of Courier object

---
+++ app/src/main/java/com/may1722/t_go/model/CourierObject.java
@@ -0,0 +1,10 @@
+package com.may1722.t_go.model;
+
+import java.util.ArrayList;
+
+public class CourierObject {
+
+    private int user_id;
+    //TODO Change to job object when added
+    private ArrayList<Object> currentJobs;
+}
