4367351a4f4cc35b410be0d6d9ca4c4b74aa4c08
Phippen <phippen@iastate.edu>
2017-02-13 19:21:44 -0600

    Add Google API files

--- app/src/main/java/com/may1722/t_go/ui/LoginActivity.java
+++ app/src/main/java/com/may1722/t_go/ui/LoginActivity.java
@@ -1,49 +1,107 @@
 package com.may1722.t_go.ui;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.may1722.t_go.R;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLEncoder;
+
 public class LoginActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public void goToSignup(View view){
+    public void goToSignup(View view) {
         //Check username and password fields and send on to SignUp if non-empty? -JRS
         Intent intent = new Intent(this, SignUpActivity.class);   //Sets up the class of the Activity we want to go to
         startActivity(intent);                                    //Starts the Activity we want to go to
     }
 
 
-
-    public void login(View view){
+    public void login(View view) {
         TextView userField = (TextView) findViewById(R.id.UserNameEditText);
         TextView passField = (TextView) findViewById(R.id.PasswordEditText);
 
         String username = userField.getText().toString();
         String password = passField.getText().toString();
 
-        if(attemptLogin(username,password)){
+        if (attemptLogin(username, password)) {
             Intent intent = new Intent(LoginActivity.this, ProfileActivity.class);
             startActivity(intent);
-        }
-        else{
+        } else {
             //display error message
         }
 
     }
 
-    private boolean attemptLogin(String user, String pass){
-        //send request to php server
+    private boolean attemptLogin(String user, String pass) {
+        URL url = new URL();
+        try {
+            String data = URLEncoder.encode("username", "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
         return true;
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Login Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
