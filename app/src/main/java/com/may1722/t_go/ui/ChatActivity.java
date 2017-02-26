5e4f39c5bbce46c03c7916b90cb2e47ddbafdabb
alxdaly <alxdaly@iastate.edu>
2017-02-26 13:07:43 -0600

    Sender's chats update on screen. Need to refresh when receiving.

--- app/src/main/java/com/may1722/t_go/ui/ChatActivity.java
+++ app/src/main/java/com/may1722/t_go/ui/ChatActivity.java
@@ -1,17 +1,27 @@
 package com.may1722.t_go.ui;
 
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.may1722.t_go.R;
 import com.may1722.t_go.model.ChatObject;
 import com.may1722.t_go.model.MessageObject;
 import com.may1722.t_go.networking.ChatRequest;
 import com.may1722.t_go.networking.SendChatRequest;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
@@ -26,6 +36,7 @@ public class ChatActivity extends AppCompatActivity {
     private EditText chatText;
     private ListView messagesListView;
     private ChatAdapter adapter;
+    private boolean ContinueChat = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +56,17 @@ public class ChatActivity extends AppCompatActivity {
             messagesListView = (ListView) findViewById(R.id.messagesListView);
             adapter = new ChatAdapter(this, chat.getMessages());
             messagesListView.setAdapter(adapter);
-//        chat.updateMessages(findChat(chatId));
+//            new MessageUpdater().execute();
+//            while(ContinueChat){
+//                chatRequest = null;
+//                chatRequest = new ChatRequest();
+//                chatRequest.execute(String.valueOf(chatId), user1, user2).get();
+//                if (chat.getMessages().size() < chatRequest.getChat().getMessages().size()) {
+//                    chat = chatRequest.getChat();
+//                    adapter.notifyDataSetChanged();
+//                }
+//                Thread.sleep(1000);
+//            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
@@ -53,16 +74,16 @@ public class ChatActivity extends AppCompatActivity {
         }
     }
 
-    public ChatObject findChat(int chatId){
+    public ChatObject findChat(int chatId) {
         return chatRequest.getChat();
     }
 
-    private void setChatWith(){
+    private void setChatWith() {
         TextView chatOtherUser = (TextView) findViewById(R.id.chatOtherUser);
         chatOtherUser.setText(" " + user2);
     }
 
-    public void sendMsg(View view){
+    public void sendMsg(View view) {
         String content = chatText.getText().toString();
         content = content.replaceAll("%", "a3rtfa");
         content = content.replaceAll("_", "b3rtfb");
@@ -71,14 +92,16 @@ public class ChatActivity extends AppCompatActivity {
         content = content.replaceAll("&", "e3rtfe");
         content = content.replace(' ', '_');
         new SendChatRequest().execute(String.valueOf(chatId), user1, content);
+        chat.addMessage(new MessageObject(user1, content, user1));  //Update screen
+        adapter.notifyDataSetChanged();
         chatText.setText("");
     }
 
-    public void addMessage(final MessageObject msg){
+    public void addMessage(final MessageObject msg) {
         runOnUiThread(new Runnable() {
 
             @Override
-            public void run(){
+            public void run() {
                 chat.addMessage(msg);
                 //
                 msgCount++;
@@ -88,7 +111,39 @@ public class ChatActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        //
+        ContinueChat = false;
         super.onBackPressed();
+    }
+
+    private class MessageUpdater extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params){
+            while(ContinueChat){
+//                ChatActivity.this.runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        chatRequest = new ChatRequest();
+//                        try {
+//                            chatRequest.execute(String.valueOf(chatId), user1, user2).get();
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        } catch (ExecutionException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                });
+                if (chat.getMessages().size() < chatRequest.getChat().getMessages().size()) {
+                    chat = chatRequest.getChat();
+                    adapter.notifyDataSetChanged();
+                }
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
     }
 }
