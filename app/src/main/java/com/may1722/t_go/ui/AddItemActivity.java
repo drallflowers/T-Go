d922c3dc1f4590f433e3bb7c9e86ba9b27f1c399
Andrewnino12 <atm12@iastate.edu>
2017-04-06 19:48:33 -0500

    Mod Functionality merge in chat encrypt

--- app/src/main/java/com/may1722/t_go/ui/AddItemActivity.java
+++ app/src/main/java/com/may1722/t_go/ui/AddItemActivity.java
@@ -1,12 +1,14 @@
 package com.may1722.t_go.ui;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
