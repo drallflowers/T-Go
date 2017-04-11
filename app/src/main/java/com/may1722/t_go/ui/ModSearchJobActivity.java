d0173b239dbf16370fef1137cffc6684a034528b
alxdaly <alxdaly@iastate.edu>
2017-04-10 22:52:23 -0500

    Created UI for ModSearchJobActivity

---
+++ app/src/main/java/com/may1722/t_go/ui/ModSearchJobActivity.java
@@ -0,0 +1,19 @@
+package com.may1722.t_go.ui;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.alxdaly.t_go.R;
+
+public class ModSearchJobActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
