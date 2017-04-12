525e279cd2f022d53bfa0a54cc494cc8aa650e27
alxdaly <alxdaly@iastate.edu>
2017-04-11 20:05:03 -0500

    Finished listView

---
+++ app/src/main/java/com/may1722/t_go/model/SearchJobAdapter.java
@@ -0,0 +1,56 @@
+package com.may1722.t_go.model;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.text.Text;
+import com.may1722.t_go.R;
+
+import java.util.List;
+
+/**
+ * Created by alxdaly on 4/11/2017.
+ */
+
+public class SearchJobAdapter extends BaseAdapter {
+    private Context context;
+    private List<SearchJobObject> jobs;
+
+    public SearchJobAdapter(Context context, List<SearchJobObject> jobs) {
+        this.context = context;
+        this.jobs = jobs;
+    }
+
+    public int getCount(){
+        return jobs.size();
+    }
+
+    @Override
+    public SearchJobObject getItem(int pos) {
+        return jobs.get(pos);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        SearchJobObject j = jobs.get(position);
+
+        LayoutInflater mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        convertView = mInflater.inflate(R.layout.list_search_job, null);
+
+        TextView jobDate = (TextView) convertView.findViewById(R.id.jobDate);
+        TextView jobId = (TextView) convertView.findViewById(R.id.jobId);
+        jobDate.setText(j.getDate().toString());
+        jobId.setText(j.getJobId());
+        return convertView;
+    }
+}
--- app/src/main/java/com/may1722/t_go/ui/ModSearchJobActivity.java
+++ app/src/main/java/com/may1722/t_go/ui/ModSearchJobActivity.java
@@ -1,19 +1,50 @@
 package com.may1722.t_go.ui;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.alxdaly.t_go.R;
+import com.may1722.t_go.R;
+import com.may1722.t_go.model.SearchJobAdapter;
+import com.may1722.t_go.model.SearchJobObject;
+
+import java.util.ArrayList;
 
 public class ModSearchJobActivity extends AppCompatActivity {
+
+    private String userID;
+    private ArrayList<SearchJobObject> jobs;
+    private ListView jobListView;
+    private SearchJobAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mod_search_job);
+        userID = getIntent().getExtras().getString("userID");
+        jobs = new ArrayList<SearchJobObject>();
+        jobListView = (ListView) findViewById(R.id.jobListView);
+        adapter = new SearchJobAdapter(this, jobs);
+        jobListView.setAdapter(adapter);
     }
 
+    public void searchJobs(View view){
+        TextView userTextView = (TextView) findViewById(R.id.MSJuserInput);
+        TextView courierTextView = (TextView) findViewById(R.id.MSJcourierInput);
+        String userName = userTextView.getText().toString();
+        String courierName = courierTextView.getText().toString();
+
+        if(userName.length() == 0){
+            Toast.makeText(ModSearchJobActivity.this, "Please submit the user's username.", Toast.LENGTH_LONG).show();
+        }
+        else if(courierName.length() == 0){
+            Toast.makeText(ModSearchJobActivity.this, "Please submit the courier's username.", Toast.LENGTH_LONG).show();
+        }
+        else{
+
+        }
+    }
 }
