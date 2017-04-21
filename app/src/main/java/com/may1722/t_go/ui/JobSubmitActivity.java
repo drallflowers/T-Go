abb7299e65c18675d75c97cde43ac7eca87443c4
Andrewnino12 <atm12@iastate.edu>
2017-04-21 00:14:06 -0500

    Merge branch 'master' into reviews2
    # Conflicts:
    #	app/src/main/java/com/may1722/t_go/ui/JobDetailsActivity.java

--- app/src/main/java/com/may1722/t_go/ui/JobSubmitActivity.java
+++ app/src/main/java/com/may1722/t_go/ui/JobSubmitActivity.java
@@ -3,30 +3,20 @@ package com.may1722.t_go.ui;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.format.DateFormat;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.may1722.t_go.R;
-import com.may1722.t_go.model.ListViewAdapter;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -41,11 +31,8 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Random;
 
 public class JobSubmitActivity extends AppCompatActivity implements
         DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
